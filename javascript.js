ff2531174585012b5d32412d03b515382148f594
Rafia Anzum <dlagrandy@gmail.com>
2024-03-18 21:19:36 +0600

    increase z index of rock paper scissor buttons container the overlay of game control section was causing rock paper scissor buttons to be un cickable

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 let gameControlSection = document.querySelector(".game-control");
 let controlBtn = document.querySelector(".game-control button");
 
+let allBtn = document.querySelector(".button-container")
+
 controlBtn.addEventListener("click", () => {
     gameControlSection.style.opacity = '0';
-    gameControlSection.style.zIndex = '-1';
+    allBtn.style.zIndex = '2';
 })
